9c30064b45e2b941554fb311743fdf75fb8e517f
Carrington <Manyuchi>
2023-07-06 16:20:07 +0200

    added functionality

--- Switch-UIKit/ViewController.swift
+++ Switch-UIKit/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let mySwitch: UISwitch = {
+        let coolSwitch = UISwitch(frame: CGRect(x: 163, y: 400, width: 70, height: 70) )
+        coolSwitch.onTintColor = UIColor.orange
+        coolSwitch.thumbTintColor = UIColor.black
+//        coolSwitch.onImage = UIImage(systemName: "lightswitch.on")
+//        coolSwitch.offImage = UIImage(systemName: "lightswitch.off")
+        coolSwitch.isOn = false
+        return coolSwitch
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(mySwitch)
+        
+        mySwitch.addTarget(self, action: #selector(switchTapped), for: .valueChanged)
+    }
+    
+     @objc private func switchTapped() {
+         if mySwitch.isOn == true {
+             view.backgroundColor = .brown
+         } else {
+             view.backgroundColor = .green
+         }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        mySwitch.setOn(true, animated: true)
     }
 
+ 
 
 }
 
